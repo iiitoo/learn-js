9d09bf9acdb76939fe03355d762687d83521d8d4
iiitoo <zy@iiitoo.com>
2018-07-05 00:48:39 +0800

    if-else学习

---
+++ if.js
@@ -0,0 +1,18 @@
+var i = 25;
+
+if (i > 25) {
+    console.log("greater then 25");
+
+} else {
+    console.log("Less than or equal to 25.");
+    
+}
+
+var a = 10;
+if (a = 10) {
+    a = 20; 
+} else {
+    a = 10;
+}
+
+console.log(a + i);
