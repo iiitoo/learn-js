ca9b79fe1d7af1513937a82947d8f7d606132094
iiitoo <zy@iiitoo.com>
2018-07-05 00:54:59 +0800

    if else判断语句学习完成

--- if.js
+++ if.js
@@ -9,10 +9,21 @@ if (i > 25) {
 }
 
 var a = 10;
-if (a = 10) {
+if (a) {
     a = 20; 
 } else {
     a = 10;
 }
 
 console.log(a + i);
+
+if (i > 25) {
+    console.log("greater then 25.");
+    
+} else if (i < 0) {
+    console.log("Less then 0.");
+    
+} else {
+    console.log("Between 0 and 25, inclusive.");
+    
+}
