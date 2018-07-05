c93f664bcb07d23eca1f177231e12658b784aef6
iiitoo <zy@iiitoo.com>
2018-07-05 19:53:21 +0800

    在学习for循环的时候，出现了i++，对这个概念我没有搞清楚，需要回学。

---
+++ for.js
@@ -0,0 +1,12 @@
+/*
+ * for语句是一种前测试循环语句， 它具有在执行循环之前初始化变量和定义循环后要执行代码的能力。
+ * 
+ * for(initialization; expression; post-loop-expression) statement
+ * 
+ */
+
+ var count = 10;
+for (var i = 0; i < count; i++) {     //出现了i++
+    console.log(i);
+    
+}
