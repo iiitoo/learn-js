0397c16a5e2f80079467d29cc0a197db4a54c7d4
iiitoo <zy@iiitoo.com>
2018-07-05 19:25:15 +0800

    赋值操作符补漏完毕，但是位赋值没有明白！

---
+++ 赋值.js
@@ -0,0 +1,33 @@
+var num = 10;
+num = num + 10;
+console.log(num);
+num += 10;
+console.log(num);
+
+num *= 10;
+console.log(num);
+
+num /= 10;
+console.log(num);
+
+num %= 10;
+console.log(num);
+
+num += 100;
+console.log(num);
+
+num -= 10;
+console.log(num);
+
+
+// 位移赋值不明白；
+
+num <<= 10;
+console.log(num);
+
+num >>= 10;
+console.log(num);
+
+num >>>= 10;
+console.log(num);
+
