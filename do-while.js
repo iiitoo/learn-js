47d8f6632946d64384e9482d46db9a3b611cdbb3
iiitoo <zy@iiitoo.com>
2018-07-05 19:29:25 +0800

    do-while语句学习完毕。

--- do-while.js
+++ do-while.js
@@ -1,12 +1,11 @@
-// var i = 0;
-// do {
-//     i += 2;
-// } while (i < 10);
+var i = 0;
+do {
+    console.log(i);
+    
+    i += 2;
+    console.log(i);
+    
+} while (i < 10);
 
-// console.log(i);
-
-var i = 3, a = 3;
-i -= 2;
-a =- 2;
 console.log(i);
-console.log(a);
+
